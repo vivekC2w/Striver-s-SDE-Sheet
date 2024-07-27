ecc3862c55e4f0a8ef4ae31a88da9262e48dc133
Vivek Hiray <vivekhiray@Viveks-Laptop.local>
2024-07-27 17:23:28 +0530

    learning hashing and sorting techniques

---
+++ BasicHashing/01_CntFreqOfArray.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void countFrequency(int arr[], int n)
+{
+    vector<bool> visited(n, false);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == true)
+            continue;
+        int count = 0;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+
+int main()
+{
+    int[] arr = {10, 5, 10, 15, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFrequency(arr, n);
+    return 0;
+}
